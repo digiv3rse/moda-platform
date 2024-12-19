f68a20aad976d7a2f70838360977e2245af69bdb
digiv3rse <kimhabork@icloud.com>
2024-12-19 13:05:56 +0700

    update container.tsx

--- components/container.tsx
+++ components/container.tsx
@@ -1,4 +1,4 @@
-export default function Container({ children }: { children: ReactNode }) {
+export default function Container({ children }) {
   return (
     <div className="container mx-auto max-w-screen-xl px-8 xl:px-5">
       {children}
